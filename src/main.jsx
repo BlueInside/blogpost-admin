dc4595d47b77b6b64d81671fb5af8d61584cdd56
Karol <kpulawski13@gmail.com>
2024-04-05 14:52:32 +0100

    Create router and RouteProvider with fallback Spinner

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './routes/App.jsx';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import BigSpinner from './components/BigSpinner.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [{}],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} fallbackElement={<BigSpinner />} />
   </React.StrictMode>
 );
