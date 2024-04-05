3a2edecd7f8f71c45e5cd4212c08d7653b2c4b7f
Karol <kpulawski13@gmail.com>
2024-04-05 14:52:41 +0100

    Create spinner component

---
+++ src/components/BigSpinner.jsx
@@ -0,0 +1,11 @@
+import '../styles/BigSpinner.css'; // Import CSS file for styling
+
+const BigSpinner = () => {
+  return (
+    <div className="big-spinner-container">
+      <div className="big-spinner"></div>
+    </div>
+  );
+};
+
+export default BigSpinner;
