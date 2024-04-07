d0cd819a4eea7333ca6e6b9ffd72d7101a7b5d3c
Karol <kpulawski13@gmail.com>
2024-04-07 12:46:47 +0100

    Create LoginForm component

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,16 +1,43 @@
-import { Form } from 'react-router-dom';
+import { Form, useActionData } from 'react-router-dom';
+import { useState } from 'react';
 
 export default function LoginForm() {
+  const errors = useActionData();
+
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+  });
+
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.currentTarget.name]: e.currentTarget.value });
+  };
+
   return (
     <>
+      {errors?.response && <p>{errors.response}</p>}
       <Form method="post">
         <div>
           <label htmlFor="username">Username: </label>
-          <input type="text" id="username" name="username" />
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={formData.username}
+            onChange={handleInputChange}
+          />
+          {errors?.username && <p>{errors.username}</p>}
         </div>
         <div>
           <label htmlFor="password">Password: </label>
-          <input type="password" id="password" name="password" />
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleInputChange}
+          />
+          {errors?.password && <p>{errors.password}</p>}
         </div>
         <div>
           <button>Submit</button>
