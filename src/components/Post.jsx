ac11209c8de217459f4bf75f56fdf7d57e7d2cf3
Karol <kpulawski13@gmail.com>
2024-04-07 15:24:54 +0100

    Navigate on post click to display more details

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,8 +1,9 @@
 import PropTypes from 'prop-types';
-
+import { useNavigate } from 'react-router-dom';
 export default function Post({ id, title, content, isPublished }) {
+  const navigate = useNavigate();
   return (
-    <div key={id}>
+    <div key={id} onClick={() => navigate(`${id}`)}>
       <h2>{title}</h2>
       <p>{content}</p>
       <p>Published: {isPublished ? '✅' : '❌'}</p>
