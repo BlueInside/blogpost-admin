b1aee6bab2fc47b0f7696c287440189cec1a804d
Karol <kpulawski13@gmail.com>
2024-04-08 09:44:23 +0100

    Creates route that displays all the posts

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './routes/App.jsx';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import BigSpinner from './components/BigSpinner.jsx';
-import Login from './Login.jsx';
+import Login from './routes/Login.jsx';
 import { actionLogin } from './loginAction.jsx';
-import Posts from './Posts.jsx';
-import { loader as postsLoader } from './postsLoader.jsx';
+import Posts from './routes/Posts.jsx';
+import { postsLoader, postLoader, commentsLoader } from './postsLoader.jsx';
+import PostDetails from './routes/PostDetails.jsx';
+import PostComments from './routes/PostComments.jsx';
+import { actionDelete } from './deleteComment.jsx';
 const router = createBrowserRouter([
   {
     path: '/',
@@ -21,6 +24,22 @@ const router = createBrowserRouter([
         path: '/posts',
         element: <Posts />,
         loader: postsLoader,
+      },
+      {
+        path: '/posts/:postId',
+        element: <PostDetails />,
+        loader: postLoader,
+        children: [
+          {
+            path: '',
+            element: <PostComments />,
+            loader: commentsLoader,
+          },
+          {
+            path: ':commentId',
+            action: actionDelete,
+          },
+        ],
       },
     ],
   },
