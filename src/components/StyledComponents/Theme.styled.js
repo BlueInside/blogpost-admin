e0a6d52bd13a182a506ca9d1dd6ca81dff8897bb
Karol <kpulawski13@gmail.com>
2024-04-08 21:04:54 +0100

    Add theme colors

---
+++ src/components/StyledComponents/Theme.styled.js
@@ -0,0 +1,14 @@
+const theme = {
+  colors: {
+    primaryBlue: '#3a00ff',
+    secondaryBlue: '#005cff',
+    primaryPink: '#ff00ea',
+    secondaryPink: '#ff55a3',
+    darkBackground: '#0e0e10',
+    lightBackground: '#1a1a1d',
+    accentYellow: '#f6ff00',
+    accentGreen: '#00ffda',
+  },
+};
+
+export default theme;
