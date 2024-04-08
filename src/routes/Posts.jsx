966fff0338796f43ff0f9133dc5237896dab3d44
Karol <kpulawski13@gmail.com>
2024-04-08 09:53:58 +0100

    Ask to log in before displaying posts

--- src/routes/Posts.jsx
+++ src/routes/Posts.jsx
@@ -1,8 +1,16 @@
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, useOutletContext } from 'react-router-dom';
 import Post from '../components/Post';
-
+import { Link } from 'react-router-dom';
 export default function Posts() {
   const posts = useLoaderData();
+  const [isLoggedIn] = useOutletContext();
+
+  if (!isLoggedIn)
+    return (
+      <h1>
+        Please log in first: <Link to={'/login'}>here</Link>
+      </h1>
+    );
   return (
     <>
       <h1>Posts: </h1>
