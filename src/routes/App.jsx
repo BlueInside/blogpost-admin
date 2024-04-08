c240be994e1c7376137939a839004955d7f7e6ff
Karol <kpulawski13@gmail.com>
2024-04-08 21:04:32 +0100

    Add GlobalStyles and ThemeProvider

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,14 +1,21 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import { useState } from 'react';
+// Styles
+import GlobalStyles from '../components/StyledComponents/Global.styled';
+import { ThemeProvider } from 'styled-components';
+import theme from '../components/StyledComponents/Theme.styled';
 
 export default function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
     <>
-      <Navbar isLoggedIn={isLoggedIn} />
-      <Outlet context={[isLoggedIn, setIsLoggedIn]} />
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Navbar isLoggedIn={isLoggedIn} />
+        <Outlet context={[isLoggedIn, setIsLoggedIn]} />
+      </ThemeProvider>
     </>
   );
 }
