462f430c6847f06c9604324af05cb26168979645
Karol <kpulawski13@gmail.com>
2024-04-08 09:45:49 +0100

    Create Login route component

---
+++ src/routes/Login.jsx
@@ -0,0 +1,21 @@
+import { useEffect } from 'react';
+import LoginForm from '../components/LoginForm';
+import { useActionData, useOutletContext, Navigate } from 'react-router-dom';
+
+export default function Login() {
+  const [isLoggedIn, setIsLoggedIn] = useOutletContext();
+  const user = useActionData();
+
+  useEffect(() => {
+    if (user?.isLogged) setIsLoggedIn(true);
+  }, [user, setIsLoggedIn]);
+
+  if (isLoggedIn) return <Navigate to={'/posts'} />;
+
+  return (
+    <>
+      <h1>Login</h1>
+      <LoginForm />
+    </>
+  );
+}
