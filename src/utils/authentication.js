acb2fb0e6861b2bfea333564bd0583d1ae7c8c2e
Karol <kpulawski13@gmail.com>
2024-04-08 09:48:35 +0100

    Authenticate helper functions

--- src/utils/authentication.js
+++ src/utils/authentication.js
@@ -3,3 +3,11 @@ export function getToken() {
   const token = localStorage.getItem('accessToken');
   return token;
 }
+
+export function isLogged() {
+  return !!localStorage.getItem('accessToken');
+}
+
+export function logOut() {
+  localStorage.removeItem('accessToken');
+}
