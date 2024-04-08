bd22943e661da7b1f9c7151380a8ef22a30241f5
Karol <kpulawski13@gmail.com>
2024-04-08 17:44:36 +0100

    Add new post route

--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,8 @@ import PostDetails from './routes/PostDetails.jsx';
 import PostComments from './routes/PostComments.jsx';
 import { actionDelete } from './deleteComment.jsx';
 import { actionUpdate } from './updateComment.jsx';
+import NewPost from './routes/NewPost.jsx';
+import { actionCreate } from './createAction.jsx';
 
 const router = createBrowserRouter([
   {
@@ -26,6 +28,11 @@ const router = createBrowserRouter([
         path: '/posts',
         element: <Posts />,
         loader: postsLoader,
+      },
+      {
+        path: '/posts/new',
+        element: <NewPost />,
+        action: actionCreate,
       },
       {
         path: '/posts/:postId',
