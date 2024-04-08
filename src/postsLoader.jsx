50f40e64a6b9c17af9377697790c949ba65e1805
Karol <kpulawski13@gmail.com>
2024-04-08 09:46:46 +0100

    Add loader for single post and loader for comments for post

--- src/postsLoader.jsx
+++ src/postsLoader.jsx
@@ -1,5 +1,25 @@
-export async function loader() {
+export async function postsLoader() {
   const url = `http://localhost:3000/posts`;
+  const options = { mode: 'cors' };
+  const res = await fetch(url, options);
+  if (res.status > 400) {
+    throw new Response('Not Found', { status: 404 });
+  }
+  return res.json();
+}
+
+export async function postLoader({ params }) {
+  const url = `http://localhost:3000/posts/${params.postId}`;
+  const options = { mode: 'cors' };
+  const res = await fetch(url, options);
+  if (res.status > 400) {
+    throw new Response('Not Found', { status: 404 });
+  }
+  return res.json();
+}
+
+export async function commentsLoader({ params }) {
+  const url = `http://localhost:3000/posts/${params.postId}/comments`;
   const options = { mode: 'cors' };
   const res = await fetch(url, options);
   if (res.status > 400) {
